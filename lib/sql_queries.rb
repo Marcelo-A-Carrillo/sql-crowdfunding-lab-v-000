1010bb3c4f4d3f3841e00cf408d682c620ea52fc
Marcelo A Carrillo <marceloacarrillo@gmail.com>
2018-01-28 23:10:28 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -23,7 +23,12 @@ def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-"Write your SQL query Here"
+  "SELECT Projects.title, (SUM(Pledges.amount) - Projects.funding_goal) AS balance
+  FROM pledges
+  INNER JOIN projects
+  ON Projects.id = Pledges.project_id
+  GROUP BY projects.title
+  HAVING balance >= 0;
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
