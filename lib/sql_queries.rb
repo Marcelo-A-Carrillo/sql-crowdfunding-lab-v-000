f20d3fc542bf155c6cbbbefdb9990cd54524232b
Marcelo A Carrillo <marceloacarrillo@gmail.com>
2018-01-28 23:11:03 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -28,8 +28,7 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
   INNER JOIN projects
   ON Projects.id = Pledges.project_id
   GROUP BY projects.title
-  HAVING balance >= 0;
-
+  HAVING balance >= 0;"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
